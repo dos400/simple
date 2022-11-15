8c68455a7cea65ea3719ff6fe66eed19d92e0001
doston <dos400dos400@gmail.com>
2022-11-15 11:26:54 +0500

    first commit

--- app/src/main/java/uz/hamroev/simple/MainActivity.kt
+++ app/src/main/java/uz/hamroev/simple/MainActivity.kt
@@ -9,5 +9,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         /*This is simple example*/
+        /*This is simple example my bro*/
+
     }
 }

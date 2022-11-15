b65b1045392f69d65e1d0b27f3791120bb83d9b5
doston <dos400dos400@gmail.com>
2022-11-15 11:23:03 +0500

    first commit

---
+++ app/src/main/java/uz/hamroev/simple/MainActivity.kt
@@ -0,0 +1,13 @@
+package uz.hamroev.simple
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        /*This is simple example*/
+    }
+}
